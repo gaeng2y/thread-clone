2847fc0b6330aaf2f9dd1264dce048e563aa87a0
gaeng2y <gaeng2y@gmail.com>
2023-08-18 01:27:37 +0900

    LoginView, RegistrationView

---
+++ threads/threads/App/threadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadsApp.swift
+//  threads
+//
+//  Created by Kyeongmo Yang on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct threadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
