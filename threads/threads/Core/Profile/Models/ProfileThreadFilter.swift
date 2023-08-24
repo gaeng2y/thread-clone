ae6f7e5b50ff83d98cdb91dda6210677107b48d6
gaeng2y <gaeng2y@gmail.com>
2023-08-25 00:52:02 +0900

    Finish ProfileView

--- threads/threads/Core/Profile/Models/ProfileThreadFilter.swift
+++ threads/threads/Core/Profile/Models/ProfileThreadFilter.swift
@@ -7,7 +7,16 @@
 
 import Foundation
 
-enum ProfileThreadFilter: Int, CaseIterable {
+enum ProfileThreadFilter: Int, CaseIterable, Identifiable {
     case threads
     case replies
+    
+    var id: Int { self.rawValue }
+    
+    var title: String {
+        switch self {
+        case .threads: return "Threads"
+        case .replies: return "Replies"
+        }
+    }
 }
--- threads/threads/Core/Profile/Views/ProfileView.swift
+++ threads/threads/Core/Profile/Views/ProfileView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    @Namespace var animation
+    
+    private var filterBarWidth: CGFloat {
+        let count = CGFloat(ProfileThreadFilter.allCases.count)
+        return UIScreen.main.bounds.width / count - 16
+    }
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
             // bio and stats
@@ -49,7 +57,40 @@ struct ProfileView: View {
                 }
                 
                 // user content list view
-                
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases) { filter in
+                            VStack {
+                                Text(filter.title)
+                                    .font(.headline)
+                                    .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .foregroundColor(.black)
+                                        .frame(width: filterBarWidth, height: 1)
+                                        .matchedGeometryEffect(id: "item", in: animation)
+                                } else {
+                                    Rectangle()
+                                        .foregroundColor(.clear)
+                                        .frame(width: filterBarWidth, height: 1)
+                                }
+                            }
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    selectedFilter = filter
+                                }
+                            }
+                        }
+                    }
+                    
+                    LazyVStack {
+                        ForEach(0 ... 10, id: \.self) { thread in
+                            ThreadCell()
+                        }
+                    }
+                }
+                .padding(.vertical, 8)
             }
         }
         .padding(.horizontal)
