1197735b859e136691797a489fae658d3a59c549
gaeng2y <gaeng2y@gmail.com>
2023-08-24 23:57:04 +0900

    ProfileView 진행중

---
+++ threads/threads/Core/Profile/Models/ProfileThreadFilter.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileThreadFilter.swift
+//  threads
+//
+//  Created by Kyeongmo Yang on 2023/08/24.
+//
+
+import Foundation
+
+enum ProfileThreadFilter: Int, CaseIterable {
+    case threads
+    case replies
+}
--- threads/threads/Core/Profile/Views/ProfileView.swift
+++ threads/threads/Core/Profile/Views/ProfileView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            // bio and stats
+            VStack(spacing: 20) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Harry Kane")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text("harry_kane")
+                                .font(.subheadline)
+                        }
+                        
+                        Text("Bayern Munchen")
+                            .font(.footnote)
+                        
+                        Text("2 followers")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    CircularProfileImageView()
+                }
+                 
+                Button {
+                    
+                } label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                // user content list view
+                
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
